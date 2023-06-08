c62c179a02e20d948f7dc6ba0f6ce22dfb818920
rkshaw20 <rkshaw2026@gmail.com>
2023-06-08 18:56:57 +0530

    style: user profile

--- src/pages/userProfile/UserProfile.jsx
+++ src/pages/userProfile/UserProfile.jsx
@@ -1,21 +1,32 @@
 import { useAuthContext } from "../../contexts/AuthContextProvider";
+import "./UserProfile.css";
 
-const UserProfile=()=>{
-    const {user,setUser}=useAuthContext()
+const UserProfile = () => {
+  const { user, setUser } = useAuthContext();
 
-    const handleLogout=()=>{
-        localStorage.removeItem('user');
-        setUser(null)
-    }
-    return(
-        <div className="profile-container">
-            <div className="profile-details">
-                <p>Full Name: {user.username}</p>
-                <p>Email: {user.email}</p>
-            </div>
-            <div className="logOut-btn"><button onClick={handleLogout}>Log Out</button></div>
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+  return (
+    <div className="profile-page">
+      <h2>Account</h2>
+      <div className="profile-container">
+        <div className="profile-details">
+          <div className="profile-details-header">
+            <h3>Profile Details</h3>
+            <button className="address-btn">Address</button>
+          </div>
+
+          <p>Full Name: {user.username}</p>
+          <p>Email: {user.email}</p>
         </div>
-    )
-}
+        <div className="logout-btn">
+          <button onClick={handleLogout}>Log Out</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UserProfile;
