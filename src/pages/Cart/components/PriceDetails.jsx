24bcdd794a88d595d27aea9e28481778caef19d3
rkshaw20 <rkshaw2026@gmail.com>
2023-07-17 12:53:52 +0530

    style: cart

--- src/pages/Cart/components/PriceDetails.jsx
+++ src/pages/Cart/components/PriceDetails.jsx
@@ -1,42 +1,46 @@
 import { Link } from "react-router-dom";
+import "./PriceDetails.css";
 
-const PriceDetails=({cart})=>{
-
-const totalItemPrice = cart.reduce((acc, { price, qty }) => acc + price * qty, 0);
-const totalDiscount= totalItemPrice * .05;
-const totalCartPrice= totalItemPrice -totalDiscount + 40;
-return(
+const PriceDetails = ({ cart }) => {
+  const totalItemPrice = cart.reduce(
+    (acc, { price, qty }) => acc + price * qty,
+    0
+  );
+  const totalDiscount = totalItemPrice * 0.05;
+  const totalCartPrice = totalItemPrice - totalDiscount + 40;
+  return (
     <div className="price-details">
-        <div className="price-details-header">
-            <h3>Price Details</h3>
+      <div className="price-details-header">
+        <h3>Price Details</h3>
+      </div>
+      <div className="price-calculation">
+        <div className="flex-container" >
+          <p className="bold-style">Price({cart.length} item)</p>
+          <p className="cal-price">₹ {totalItemPrice}</p>
         </div>
-        <div className="price-calculation">
-            <li>
-                <ul><p className="bold-style">Price({cart.length} item)</p>
-                <p className="cal-price">₹ {totalItemPrice}</p>
-                </ul>
-                <ul>
-                    <p className="bold-style">Discount (5% off)</p>
-                    <p className="cal-price">-₹ {totalDiscount.toFixed()}</p>
-                </ul>
-                <ul>
-                    <p className="bold-style">Delivery Fee</p>
-                    <p className="cal-price">₹ 40</p>
-                </ul>
-            </li>
-            <hr />
+        <div className="flex-container">
+          <p className="bold-style">Discount (5% off)</p>
+          <p className="cal-price">-₹ {totalDiscount.toFixed()}</p>
         </div>
-        <ul className="total-amount">
-            <p className="bold-style">Total Amount</p>
-            <p className="cal-price">₹ {totalCartPrice.toFixed()}</p>
-        </ul>
-        <div className="price-details-checkout">
-            <Link to='/checkoutPage'>
-            <button className="checkout-btn">Checkout</button>
+        <div className="flex-container">
+          <p className="bold-style">Delivery Fee</p>
+          <p className="cal-price">₹ 40</p>
+        </div>
 
-            </Link>
-        </div>
+        <hr />
+        <div className="total-amount flex-container">
+        <p className="bold-style">Total Amount</p>
+        <p className="cal-price">₹ {totalCartPrice.toFixed()}</p>
+      </div>
+      </div >
+      
+
+      <div className="price-details-checkout">
+        <Link to="/checkoutPage">
+          <button className="checkout-btn">Checkout</button>
+        </Link>
+      </div>
     </div>
-)
-}
+  );
+};
 export default PriceDetails;
